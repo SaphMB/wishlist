0168d449127e76bc2a57697edce316b0cb864330
SaphMB <saphmb@gmail.com>
2016-08-02 10:44:38 +0100

    init

---
+++ script.js
@@ -0,0 +1,18 @@
+/* Exercise 1: Wish list */
+function addToList(item) {
+	var script = '<li>' + item + '<span class=\'label pending\'>Pending</span>' + '</li>';
+	$('#items').append(script);
+}
+
+$(document).on('click', '#add-to-list', function () {
+	addToList($('#item').val());
+	$('#item').val('');
+	$('#item').focus();
+});
+
+$(document).on('click', '.pending', function () {
+	var li_node = $(this).parent();
+	li_node.append('<span class\'label success\'>Done!</span>');
+	li_node.addClass('completed');
+	$(this).remove();
+});
